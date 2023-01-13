5446d5c73212a6de830f8662679566d48feab62c
Tamzid Israk <tamzidisrak12@gmail.com>
2023-01-13 22:52:02 +0600

    Coroutine
    Kotlin Coroutines Basics

--- app/src/main/java/com/example/androidcourutine/MainActivity.kt
+++ app/src/main/java/com/example/androidcourutine/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.androidcourutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.coroutines.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG:String = "KotlinFun"
+    lateinit var counterText:TextView
+    var counterNum:Int =0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        counterText = findViewById(R.id.textView)
+        Log.d(TAG, "Current Thread name is ${Thread.currentThread().name}")
+
+    }
+
+    fun fCounter(view: View) {
+        counterNum++
+        Log.d(TAG, "Current Thread name is ${Thread.currentThread().name}")
+        counterText.text=counterNum.toString()
+    }
+
+    fun doAction(view: View) {
+        /*Coroutine run on top of thread & its lightweight
+        * Dispatchers is a way to define threads on which Coroutines are executed
+        * Predefined dispatchers - dispatchers.io for i/o operation \\ dispatchers.Main for running on main thread \\ dispatchers.default for default operation
+        * CoroutineScope -> lifetime of Coroutine
+        * GlobalScope -> attached with whole application
+        * MainScope -> attached with MainActivity*/
+
+
+
+
+       CoroutineScope(Dispatchers.IO).launch {
+           Log.d(TAG,"1-${Thread.currentThread().name}")
+       }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            Log.d(TAG,"2-${Thread.currentThread().name}")
+        }
+
+        MainScope().launch(Dispatchers.Default) {
+            Log.d(TAG,"3-${Thread.currentThread().name}")
+        }
+    }
+
+
+    private fun executeLongRunningTask(){
+        for ( i in 1.. 1000000000L){
+           Log.d(TAG,i.toString())
+        }
     }
 }
