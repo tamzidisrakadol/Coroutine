7cb7e8562cd6cbb680082546f330636e3a471429
Tamzid Israk <tamzidisrak12@gmail.com>
2023-01-17 03:30:46 +0600

    Coroutine
    Kotlin Coroutines Basics
    Suspending Functions & Suspend Modifier
    Launch vs Async
    Kotlin Coroutine Jobs & Cancellation
    Kotlin Coroutine WithContext & runBlocking Functions
    ViewModelScope & LifecycleScope
    Coroutines with Retrofit
    
    Stream
    Kotlin Flows & Channels Introduction
    Flow Builder, Cold, Cancellation
    Flow Operators - Terminal + Map, Filter Buffer Operators

--- app/src/main/java/com/example/androidcourutine/MainActivity3.kt
+++ app/src/main/java/com/example/androidcourutine/MainActivity3.kt
@@ -11,9 +11,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlin.system.measureTimeMillis
 
@@ -22,18 +20,18 @@ class MainActivity3 : AppCompatActivity() {
 
     //creating channel
     val channel = Channel<Int>()
-    val TAG:String ="Flows"
+    val TAG: String = "Flows"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
-     /*   suspend function use cases-> storing values in database, network call, Doing task that return single value
-        Stream function use case -> Video Streaming, Fm Radio, Mobile sending audio signals to bluetooth speaker
-        kotlin has asynchronous stream support using channels and flow
-        * Channel -> Send & Receive -> Channels are hot-> Channels will continuously produce data if there is no consumer exist ex:Cinema hall
-        * FLows -> Emit  and Collect -> Flows are Cold -> Flows will only product the data when there is consumer ex: netflix
-        **/
+        /*   suspend function use cases-> storing values in database, network call, Doing task that return single value
+           Stream function use case -> Video Streaming, Fm Radio, Mobile sending audio signals to bluetooth speaker
+           kotlin has asynchronous stream support using channels and flow
+           * Channel -> Send & Receive -> Channels are hot-> Channels will continuously produce data if there is no consumer exist ex:Cinema hall
+           * FLows -> Emit  and Collect -> Flows are Cold -> Flows will only product the data when there is consumer ex: netflix
+           **/
         producer()
         consumer()
 
@@ -41,25 +39,52 @@ class MainActivity3 : AppCompatActivity() {
             //this "data" variable  is a consumer \\ if there is no consumer then producer not produce data
             val data: Flow<Int> = listProducer()
             val time = measureTimeMillis {
-                data.collect{
-                    Log.d(TAG,"1st consumer ${it.toString()}")
+                data.collect {
+                    Log.d(TAG, "1st consumer ${it.toString()}")
                 }
             }
-           Log.d(TAG,"time taken $time")
+            Log.d(TAG, "time taken $time")
         }
 
         lifecycleScope.launch {
             //can create multiple consumer -> there will be not waste of data for delay
+
             val data2 = listProducer()
             delay(2000L)
-            data2.collect{
-                Log.d(TAG,"2nd consumer ${it.toString()}")
-            }
+            //event of consumer
+
+                data2.map {
+                    it * 2
+                }
+                    .filter {
+                        it < 200
+                    }
+                    .buffer(3)
+                    .onStart {
+                        //can add value on start
+                        emit(-2)
+                        Log.d(TAG,"started")
+                    }
+                    .onCompletion {
+                        //can also add value just before complete
+                        emit(200)
+                        Log.d(TAG,"completed")
+                    }
+                    .onEach {
+                        Log.d(TAG,"About to emit $it")
+                    }
+                    .collect {
+                        Log.d(TAG, "2nd consumer ${it.toString()}")
+                    }
+
+
+
+
         }
 
     }
 
-    fun producer(){ //producing data
+    fun producer() { //producing data
         //channel.send will run on Coroutine
         CoroutineScope(Dispatchers.Main).launch {
             channel.send(1)
@@ -67,17 +92,17 @@ class MainActivity3 : AppCompatActivity() {
         }
     }
 
-    fun consumer(){ //receiving data
+    fun consumer() { //receiving data
         //channel.Receive will run on Coroutine
         CoroutineScope(Dispatchers.Main).launch {
-            Log.d(TAG,channel.receive().toString())
-            Log.d(TAG,channel.receive().toString())
+            Log.d(TAG, channel.receive().toString())
+            Log.d(TAG, channel.receive().toString())
         }
     }
 
     //listProducer is a flowing suspending function->it will producing data in every 1000s
     fun listProducer() = flow<Int> {
-        val list = listOf<Int>(11,22,33,44,55,66,77,88,99)
+        val list = listOf<Int>(11, 22, 33, 44, 55, 66, 77, 88, 99)
         list.forEach {
             delay(1000L)
             emit(it)
