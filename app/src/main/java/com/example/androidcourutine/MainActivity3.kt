bb340b89f22bf3a917be82fab00a1790ceaa650b
Tamzid Israk <tamzidisrak12@gmail.com>
2023-01-17 03:01:15 +0600

    Coroutine
    Kotlin Coroutines Basics
    Suspending Functions & Suspend Modifier
    Launch vs Async
    Kotlin Coroutine Jobs & Cancellation
    Kotlin Coroutine WithContext & runBlocking Functions
    ViewModelScope & LifecycleScope
    Coroutines with Retrofit
    
    Stream
    Kotlin Flows & Channels Introduction
    Flow Builder, Cold, Cancellation

--- app/src/main/java/com/example/androidcourutine/MainActivity3.kt
+++ app/src/main/java/com/example/androidcourutine/MainActivity3.kt
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlin.system.measureTimeMillis
 
@@ -31,6 +37,26 @@ class MainActivity3 : AppCompatActivity() {
         producer()
         consumer()
 
+        lifecycleScope.launch {
+            //this "data" variable  is a consumer \\ if there is no consumer then producer not produce data
+            val data: Flow<Int> = listProducer()
+            val time = measureTimeMillis {
+                data.collect{
+                    Log.d(TAG,"1st consumer ${it.toString()}")
+                }
+            }
+           Log.d(TAG,"time taken $time")
+        }
+
+        lifecycleScope.launch {
+            //can create multiple consumer -> there will be not waste of data for delay
+            val data2 = listProducer()
+            delay(2000L)
+            data2.collect{
+                Log.d(TAG,"2nd consumer ${it.toString()}")
+            }
+        }
+
     }
 
     fun producer(){ //producing data
@@ -46,6 +72,15 @@ class MainActivity3 : AppCompatActivity() {
         CoroutineScope(Dispatchers.Main).launch {
             Log.d(TAG,channel.receive().toString())
             Log.d(TAG,channel.receive().toString())
+        }
+    }
+
+    //listProducer is a flowing suspending function->it will producing data in every 1000s
+    fun listProducer() = flow<Int> {
+        val list = listOf<Int>(11,22,33,44,55,66,77,88,99)
+        list.forEach {
+            delay(1000L)
+            emit(it)
         }
     }
 
